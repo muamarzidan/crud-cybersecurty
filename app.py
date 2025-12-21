d7963cbc869129b178ce55c1b3957b759cc658fb
SKYLINERGB <gtxskyline22@gmail.com>
2025-12-21 22:45:34 +0700

    erbaikan

--- app.py
+++ app.py
@@ -204,4 +204,6 @@ def edit_student(id):
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    # Mengubah debug=True menjadi debug=False untuk keamanan
+    # app.run(host='0.0.0.0', port=5000, debug=True) <- Kode Lama
+    app.run(host='0.0.0.0', port=5000, debug=False) # <- Kode Baru
